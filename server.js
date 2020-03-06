f1f457c255924d3ff24a7404e8b36442ebd98e37
Urs Plattner <urs.plattner@ipi.ch>
2020-03-06 12:16:21 +0100

    PLU-1: test

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
     
+var http = require('http');
+    
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
